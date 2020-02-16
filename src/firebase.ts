ff9aed1705bb26174a6182ae47e15ce21a7f8d15
Jason Daly <jason@deefour.me>
2020-02-16 15:40:14 -0500

    use environment config for firebase client

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,9 +3,15 @@ import "firebase/firestore";
 
 import firebase from "firebase/app";
 
-export const app = firebase.initializeApp(
-  require("../credentials.client.json")
-);
+export const app = firebase.initializeApp({
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
+});
 
 export default firebase;
 export const firestore = app.firestore();
