5568cb2378f4927b0c92c73d14386842b68802c5
Jason Daly <jason@deefour.me>
2020-02-17 20:26:27 -0500

    add purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,16 @@
+/**
+ * @link https://tailwindcss.com/docs/controlling-file-size/#setting-up-purgecss
+ */
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  content: ["./src/**/*.vue"],
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+});
+
 module.exports = {
   plugins: [
     require("tailwindcss"),
     require("autoprefixer"),
-    require("postcss-nested")
+    require("postcss-nested"),
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : [])
   ]
 };
