7774ff7f29818166c4d590afefe0aa6a29cbed9f
Jason Daly <jason@deefour.me>
2020-02-17 20:58:49 -0500

    create a firebase messaging reference

--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -4,3 +4,5 @@ importScripts("https://www.gstatic.com/firebasejs/7.8.1/firebase-messaging.js");
 firebase.initializeApp({
   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 });
+
+const messaging = firebase.messaging();
